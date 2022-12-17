612d851f3eb2a6fe36274255f70e575f4961608f
maurice <akpokeremaurice@gmail.com>
2022-12-16 17:13:27 -0800

    commiting more nested project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - functions that multiplies 2 intergers
+ * @a: first num
+ * @b: second num
+ * Return: product of 2 integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
