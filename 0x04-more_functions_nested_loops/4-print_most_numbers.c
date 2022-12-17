2d3388517bf7ec71b12b95dd43a13006e2ee0312
maurice <akpokeremaurice@gmail.com>
2022-12-16 20:51:02 -0800

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#iinclude "main.h"
 
 /**
 * print_most_numbers - prints from 0 to 9 without 2 and 4
@@ -8,6 +8,7 @@
 void print_most_numbers(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
