4c2eb909dbcb7d91edcb443fe377af137135b417
maurice <akpokeremaurice@gmail.com>
2022-12-19 17:43:25 -0800

    array commitment

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the n value to 98
+ * @n: inputted number
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This projects shows how to use arrays, pointers and strings in C programming
