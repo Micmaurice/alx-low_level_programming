50217876a745aca2d167cc4a57195bb3e1c36e33
maurice <akpokeremaurice@gmail.com>
2022-12-19 20:44:12 -0800

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: prints half of string and returns to 0
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n;
+
+	while (str[i] != '\0')
+		i++;
+	if (i + 1 % 2 != '0')
+		n = (i - 1) / 2;
+	else
+		n = (i / 2)
+			n++;
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+		_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of array of integers
+ * @a: first integer
+ * @n: second integer
+ * Return: if success, return 0
+ */
+
+void print_print_array(int *a, int n);
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+			print("\n");
+}
