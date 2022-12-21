51997d8eedbcf61ac3896721137f7bf01449be4c
maurice <akpokeremaurice@gmail.com>
2022-12-20 21:17:29 -0800

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,20 @@
  */
 
 void print_array(int *a, int n)
-
+i
 {
 	int x;
 
-	for (x = 0; x < n; x++)
-		if (x != n - 1)
+	for 
+		(x = 0; x < n; x++)
+		if 
+		{
+			(x != n - 1)
 			printf("%d, ", a[x]);
+		}
 		else
+		{
 			printf("%d", a[x]);
 			printf("\n");
+		}
 }
