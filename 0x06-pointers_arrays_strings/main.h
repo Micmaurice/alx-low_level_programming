4320b330352ce4766e9208b5a85ce26af7977c76
maurice <akpokeremaurice@gmail.com>
2022-12-22 20:38:06 -0800

    more pointers and arrays

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+int _putchar(char);
 char *_strcat(char *dest, char *scr);
 char *_strncat(char *dest, char *scr, int n);
 char *_strncpy(char *dest, char *src, int n);
