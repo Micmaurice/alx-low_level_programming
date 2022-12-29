8b21cb56d14a69e33ea1b039f0af11b71b2b3a55
maurice <akpokeremaurice@gmail.com>
2022-12-28 21:04:43 -0800

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+/**
+ * main - mix of lower and upper case in c
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
