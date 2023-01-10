6e0d4366c61f010499684332fe8c022fa014b1da
Micmaurice <akpokeremaurice@gmail.com>
2023-01-09 19:46:15 -0800

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * _memcpy - fills memory with another buffer
+ * @dest: source string
+ * @src: string for filling
+ * @n: lenght
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function
+ * @s: parameter
+ * @accept: parameter
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		for (; *accept; accept++)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			s++;
+		}
+	}
+	return (NULL);
+}
