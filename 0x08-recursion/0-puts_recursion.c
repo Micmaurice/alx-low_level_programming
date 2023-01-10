6f961a8321811d362b5d0a8f2d7a2f2397b87cad
Micmaurice <akpokeremaurice@gmail.com>
2023-01-10 06:01:39 -0800

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: character
+ * Return:
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\n')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
